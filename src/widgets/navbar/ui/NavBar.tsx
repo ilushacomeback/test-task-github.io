c6bff3430814a72f96914b93dc3544400921e3a1
ilushacomeback <vip.ilya.rud.2016@mail.com>
2024-05-21 21:08:02 +0300

    test

--- src/widgets/navbar/ui/NavBar.tsx
+++ src/widgets/navbar/ui/NavBar.tsx
@@ -1,10 +1,12 @@
 import { AppBar, Container, Toolbar, Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { actions } from '../../../app/model';
 
 export const NavBar = () => {
   const token = useAppSelector((state) => state.authReducer.token)
   const dispatch = useAppDispatch()
+  const navigate = useNavigate()
   const logout = actions.logout
   return (
     <AppBar component="nav">
@@ -14,7 +16,7 @@ export const NavBar = () => {
           sx={{ justifyContent: 'flex-end', flexWrap: 'wrap' }}
         >
           {!token ? (
-            <Button color="inherit" sx={{ border: '0' }} href={`/login`}>
+            <Button color="inherit" sx={{ border: '0' }} onClick={() => navigate('/login')}>
               <Typography variant="h6">login</Typography>
             </Button>
           ) : (
