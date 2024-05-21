525254d03314ee82ddcd1f1c000ec944b0072499
ilushacomeback <vip.ilya.rud.2016@mail.com>
2024-05-21 21:13:09 +0300

    test

--- src/widgets/navbar/ui/NavBar.tsx
+++ src/widgets/navbar/ui/NavBar.tsx
@@ -1,13 +1,11 @@
 import { AppBar, Container, Toolbar, Button, Typography } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { actions } from '../../../app/model';
 
 export const NavBar = () => {
-  const token = useAppSelector((state) => state.authReducer.token)
-  const dispatch = useAppDispatch()
-  const navigate = useNavigate()
-  const logout = actions.logout
+  const token = useAppSelector((state) => state.authReducer.token);
+  const dispatch = useAppDispatch();
+  const logout = actions.logout;
   return (
     <AppBar component="nav">
       <Container maxWidth="xl">
@@ -15,12 +13,12 @@ export const NavBar = () => {
           disableGutters
           sx={{ justifyContent: 'flex-end', flexWrap: 'wrap' }}
         >
-          {!token ? (
-            <Button color="inherit" sx={{ border: '0' }} onClick={() => navigate('/login')}>
-              <Typography variant="h6">login</Typography>
-            </Button>
-          ) : (
-            <Button color="inherit" sx={{ border: '0' }} onClick={() => dispatch(logout())}>
+          {token && (
+            <Button
+              color="inherit"
+              sx={{ border: '0' }}
+              onClick={() => dispatch(logout())}
+            >
               <Typography variant="h6">Exit</Typography>
             </Button>
           )}
